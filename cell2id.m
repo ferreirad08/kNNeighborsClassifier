7157211baa807b81c54e81782ca046a65a6925be
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2019-01-25 22:59:01 -0200

    Update cell2id.m

--- cell2id.m
+++ cell2id.m
@@ -3,8 +3,8 @@ function [Y,C] = cell2id(Y,C)
     Y = zeros(size(Ycell,1),1);
     if nargin < 2, C = unique(Ycell); end
     for i = 1:size(Ycell,1)
-        for j = 1:size(C,1)
-            if strcmp(Ycell(i),C(j)), Y(i) = j; end
+        for id = 1:size(C,1)
+            if strcmp(Ycell(i),C(id)), Y(i) = id; end
         end
     end
 end
