11bb8da5cc5e8c6b89482b60b39ffe9b9729ab61
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2019-01-28 02:19:30 -0200

    Update predict_knn.m

--- predict_knn.m
+++ predict_knn.m
@@ -70,3 +70,11 @@ end
 
 if tf, label = C(label); end
 end
+
+function Yid = cell2id(Ynew,C)    
+Yid = zeros(size(Ynew,1),1);
+for i = 1:numel(C);
+    I = find(strcmp(Ynew,C(i)));
+    Yid(I) = i;
+end
+end
