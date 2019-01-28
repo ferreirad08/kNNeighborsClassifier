ab65dd72b384b73fbf735767b0dbad931202e8ba
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2019-01-28 02:09:19 -0200

    Update cell2id.m

--- cell2id.m
+++ cell2id.m
@@ -1,8 +1,7 @@
-function Yid = cell2id(Ynew,C)
-    Yid = zeros(size(Ynew,1),1);
-    for i = 1:size(Ynew,1)
-        for id = 1:size(C,1)
-            if strcmp(Ynew(i),C(id)), Yid(i) = id; end
-        end
-    end
+function Yid = cell2id(Ynew,C)    
+     Yid = zeros(size(Ynew,1),1);
+     for i = 1:numel(C);
+         I = find(strcmp(Ynew,C(i)));
+         Yid(I) = i;
+     end
 end
