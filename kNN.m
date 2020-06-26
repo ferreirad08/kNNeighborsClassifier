64e9379734607d3d28b66f90a9aff18a052b0b66
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-06-26 09:19:25 -0400

    Update kNN.m

--- kNN.m
+++ kNN.m
@@ -1,20 +1,21 @@
-function [Ypred,I] = kNN(X,Y,Xnew,k)
+function [Ypred,indices,distances] = kNN(X,Y,Xnew,k,p)
 [C,~,Y] = unique(Y);
 
-% Euclidean distance between two points
-distances = pdist2(X,Xnew);
+% Distance between two points
+distances = dsb_utilities.cdist(Xnew,X,p);
 % Sort the distances in ascending order and check the k nearest training labels
-[~,I] = sort(distances);
-I = I(1:k,:);
-Ynearest = Y(I);
+[distances,indices] = sort(distances,2);
+distances = distances(:,1:k);
+indices = indices(:,1:k);
+
+Ynearest = Y(indices)';
 % Frequencies of the k nearest training labels
 N = histc(Ynearest,1:max(Y));
-[m,P] = size(N);
-frequencies = N(Ynearest+repmat(0:m:P*m-m,k,1));
+[n_class,P] = size(N);
+frequencies = N(Ynearest + repmat(0:n_class:P*n_class-n_class,k,1));
 % Nearest training label with maximum frequency (if duplicated, check the nearest training instance)
 [~,J] = max(frequencies);
 Ypred = Ynearest(J+(0:k:P*k-k));
 
 Ypred = C(Ypred);
-I = I';
 end
