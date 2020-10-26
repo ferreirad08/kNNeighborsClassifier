b20f444fcf7d7c82fee35daf72ef81cda5f5b70e
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-10-26 11:32:11 -0400

    Add files via upload

--- kNNeighbors.m
+++ kNNeighbors.m
@@ -1,13 +1,40 @@
-function Ypred = kNNeighbors(X,Y,Xnew,k)
+classdef kNNeighbors
 %k-Nearest Neighbors (kNN)
 %
 % Author: M.Sc. David Ferreira - Federal University of Amazonas
 % Contact: ferreirad08@gmail.com
 % Date: October 2020
 
-[C,~,Y] = unique(Y); % Numeric labels
-distances = pdist2(X,Xnew); % Euclidean distance matrix
-[distances,indices] = sort(distances); % Ordered distances
-Ynearest = Y(indices(1:k,:)); % k nearest labels
-Ypred = C(mode(Ynearest)); % Most frequent label
+properties
+    k = 5
+    metric = 'euclidean'
+    X
+    name_labels
+    Y
+end
+methods
+    function obj = kNNeighbors(k,metric)
+        if nargin > 0
+            obj.k = k;
+        end
+        if nargin > 1
+            obj.metric = metric;
+        end
+    end
+    function obj = fit(obj,X,Y)
+        obj.X = X;
+        [obj.name_labels,~,obj.Y] = unique(Y); % Numeric labels
+    end
+    function [distances,indices] = find(obj,Xnew)
+        distances = pdist2(obj.X,Xnew,obj.metric); % Euclidean distance matrix
+        [distances,indices] = sort(distances); % Ordered distances
+        distances = distances(1:obj.k,:);
+        indices = indices(1:obj.k,:);
+    end
+    function Ypred = predict(obj,Xnew)
+        [~,indices] = find(obj,Xnew);
+        Ynearest = obj.Y(indices); % k-nearest labels
+        Ypred = obj.name_labels(mode(Ynearest)); % Most frequent label
+    end
+end
 end
