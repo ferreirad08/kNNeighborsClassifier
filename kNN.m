e788a76d2fc0a5d977ad92bf4ffe6e713f5b986d
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-10-26 02:14:07 -0400

    Update kNN.m

--- kNN.m
+++ kNN.m
@@ -1,21 +1,14 @@
-function [Ypred,indices,distances] = kNN(X,Y,Xnew,k,p)
-[C,~,Y] = unique(Y);
+function Ypred = kNNeighbors(X,Y,Xnew,k)
+%k-Nearest Neighbors (kNN)
+%
+% David Alan de Oliveira Ferreira (http://lattes.cnpq.br/3863655668683045)
+% D.Sc. student in Electrical Engineering from the Federal University of
+% Amazonas 
+% e-mail: ferreirad08@gmail.com
 
-% Distance between two points
-distances = dsb_utilities.cdist(Xnew,X,p);
-% Sort the distances in ascending order and check the k nearest training labels
-[distances,indices] = sort(distances,2);
-distances = distances(:,1:k);
-indices = indices(:,1:k);
-
-Ynearest = Y(indices)';
-% Frequencies of the k nearest training labels
-N = histc(Ynearest,1:max(Y));
-[n_class,P] = size(N);
-frequencies = N(Ynearest + repmat(0:n_class:P*n_class-n_class,k,1));
-% Nearest training label with maximum frequency (if duplicated, check the nearest training instance)
-[~,J] = max(frequencies);
-Ypred = Ynearest(J+(0:k:P*k-k));
-
-Ypred = C(Ypred);
+[C,~,Y] = unique(Y); % Numeric labels
+distances = pdist2(X,Xnew); % Distance between two vectors
+[distances,indices] = sort(distances); % Ordered distances
+Ynearest = Y(indices(1:k,:)); % k nearest labels
+Ypred = C(mode(Ynearest)); % Most frequent label
 end
